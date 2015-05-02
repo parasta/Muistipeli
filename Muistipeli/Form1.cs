89490ac03dfa87c261024976a5e14bcc260841a6
Juha Pärkkä <juha.parkka@gmail.com>
2015-05-02 18:24:59 +0300

    Tarkistaa loppuiko peli ja tekee lopputoimet

--- Muistipeli/Form1.cs
+++ Muistipeli/Form1.cs
@@ -99,6 +99,10 @@ namespace Muistipeli
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
+                // Check to see if the player won
+                CheckForWinner();
+
+
                 // If the player clicked two matching icons, keep them  
                 // black and reset firstClicked and secondClicked  
                 // so the player can click another icon 
@@ -133,6 +137,33 @@ namespace Muistipeli
             // clicked, the program knows it's the first click
             firstClicked = null;
             secondClicked = null;
+        }
+
+        /// <summary> 
+        /// Check every icon to see if it is matched, by  
+        /// comparing its foreground color to its background color.  
+        /// If all of the icons are matched, the player wins 
+        /// </summary> 
+        private void CheckForWinner()
+        {
+            // Go through all of the labels in the TableLayoutPanel,  
+            // checking each one to see if its icon is matched 
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            // If the loop didn’t return, it didn't find 
+            // any unmatched icons 
+            // That means the user won. Show a message and close the form
+            MessageBox.Show("Löysit kaikki parit !", "Hienoa!");
+            Close();
         }
 
 
